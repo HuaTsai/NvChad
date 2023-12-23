b71669b2a1ed454b230f7da7fc4fe631755a1f05
HuaTsai <huatsai.eed07g@nctu.edu.tw>
2023-12-23 23:33:15 +0800

    feat: cpplint

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -4,7 +4,7 @@ local null_ls = require("null-ls")
 local opts = {
   sources = {
     null_ls.builtins.formatting.clang_format,
-    null_ls.builtins.diagnostics.clang_check,
+    null_ls.builtins.diagnostics.cpplint,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.diagnostics.eslint,
   },
